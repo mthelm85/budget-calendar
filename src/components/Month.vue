33fef596c4fad6beaf708027a3f77310ddc9c7fc
Matt Helm <mthelm85@gmail.com>
2018-08-28 20:02:49 -0500

    added transition and comments to explain functions

--- src/components/Month.vue
+++ src/components/Month.vue
@@ -1,66 +1,71 @@
 <template lang="html">
-  <div class="container-fluid">
-    <div class="row mt-2">
-      <div class="col">
-        <span class="display-4">{{ month }}</span>
-        <span class="float-right mt-4" @click="toNext">{{ nextMonth }}</span>
-        <table class="table table-bordered mt-3">
-          <thead class="thead-dark">
-            <tr>
-              <th class="day">Sunday</th>
-              <th class="day">Monday</th>
-              <th class="day">Tuesday</th>
-              <th class="day">Wednesday</th>
-              <th class="day">Thursday</th>
-              <th class="day">Friday</th>
-              <th class="day">Saturday</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr class="week">
-              <td class="lead" ref="day1">{{ weekOne(firstDay - 0) }}</td>
-              <td class="lead">{{ weekOne(firstDay - 1) }}</td>
-              <td class="lead">{{ weekOne(firstDay - 2) }}</td>
-              <td class="lead">{{ weekOne(firstDay - 3) }}</td>
-              <td class="lead">{{ weekOne(firstDay - 4) }}</td>
-              <td class="lead">{{ weekOne(firstDay - 5) }}</td>
-              <td class="lead">{{ weekOne(firstDay - 6) }}</td>
-            </tr>
-            <tr class="week">
-              <td v-for="day in weekTwo" :key="day" class="lead">{{ day }}</td>
-            </tr>
-            <tr class="week">
-              <td v-for="day in weekThree" :key="day" class="lead">{{ day }}</td>
-            </tr>
-            <tr class="week">
-              <td v-for="day in weekFour" :key="day" class="lead">{{ day }}</td>
-            </tr>
-            <tr v-if="!extraRow" class="week">
-              <td class="lead">{{ weekLast(lastDay - 0) }}</td>
-              <td class="lead">{{ weekLast(lastDay - 1) }}</td>
-              <td class="lead">{{ weekLast(lastDay - 2) }}</td>
-              <td class="lead">{{ weekLast(lastDay - 3) }}</td>
-              <td class="lead">{{ weekLast(lastDay - 4) }}</td>
-              <td class="lead">{{ weekLast(lastDay - 5) }}</td>
-              <td class="lead">{{ weekLast(lastDay - 6) }}</td>
-            </tr>
-            <tr v-if="extraRow" class="week">
-              <td v-for="day in weekFiveOfSix" :key="day" class="lead">{{ day }}</td>
-            </tr>
-            <tr v-if="extraRow" class="week">
-              <td class="lead">{{ weekLast(lastDay - 0) }}</td>
-              <td class="lead">{{ weekLast(lastDay - 1) }}</td>
-              <td class="lead">{{ weekLast(lastDay - 2) }}</td>
-              <td class="lead">{{ weekLast(lastDay - 3) }}</td>
-              <td class="lead">{{ weekLast(lastDay - 4) }}</td>
-              <td class="lead">{{ weekLast(lastDay - 5) }}</td>
-              <td class="lead">{{ weekLast(lastDay - 6) }}</td>
-            </tr>
-          </tbody>
-        </table>
+  <transition name="fade" mode="out-in">
+    <div class="container-fluid" :key="monthIndex">
+      <div class="row mt-2">
+        <div class="col">
+          <span class="display-4">{{ month }}</span>
+          <span class="float-right mt-4">
+            <button class="btn btn-outline-dark" @click="monthIndex--">{{ prevMonth }}</button>
+            <button class="btn btn-outline-dark" @click="monthIndex++">{{ nextMonth }}</button>
+          </span>
+          <table class="table table-bordered mt-3">
+            <thead class="thead-dark">
+              <tr>
+                <th class="day">Sunday</th>
+                <th class="day">Monday</th>
+                <th class="day">Tuesday</th>
+                <th class="day">Wednesday</th>
+                <th class="day">Thursday</th>
+                <th class="day">Friday</th>
+                <th class="day">Saturday</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr class="week">
+                <td class="lead" ref="day1">{{ weekOne(firstDay - 0) }}</td>
+                <td class="lead">{{ weekOne(firstDay - 1) }}</td>
+                <td class="lead">{{ weekOne(firstDay - 2) }}</td>
+                <td class="lead">{{ weekOne(firstDay - 3) }}</td>
+                <td class="lead">{{ weekOne(firstDay - 4) }}</td>
+                <td class="lead">{{ weekOne(firstDay - 5) }}</td>
+                <td class="lead">{{ weekOne(firstDay - 6) }}</td>
+              </tr>
+              <tr class="week">
+                <td v-for="day in weekTwo" :key="day" class="lead">{{ day }}</td>
+              </tr>
+              <tr class="week">
+                <td v-for="day in weekThree" :key="day" class="lead">{{ day }}</td>
+              </tr>
+              <tr class="week">
+                <td v-for="day in weekFour" :key="day" class="lead">{{ day }}</td>
+              </tr>
+              <tr v-if="!extraRow" class="week">
+                <td class="lead">{{ weekLast(lastDay - 0) }}</td>
+                <td class="lead">{{ weekLast(lastDay - 1) }}</td>
+                <td class="lead">{{ weekLast(lastDay - 2) }}</td>
+                <td class="lead">{{ weekLast(lastDay - 3) }}</td>
+                <td class="lead">{{ weekLast(lastDay - 4) }}</td>
+                <td class="lead">{{ weekLast(lastDay - 5) }}</td>
+                <td class="lead">{{ weekLast(lastDay - 6) }}</td>
+              </tr>
+              <tr v-if="extraRow" class="week">
+                <td v-for="day in weekFiveOfSix" :key="day" class="lead">{{ day }}</td>
+              </tr>
+              <tr v-if="extraRow" class="week">
+                <td class="lead">{{ weekLast(lastDay - 0) }}</td>
+                <td class="lead">{{ weekLast(lastDay - 1) }}</td>
+                <td class="lead">{{ weekLast(lastDay - 2) }}</td>
+                <td class="lead">{{ weekLast(lastDay - 3) }}</td>
+                <td class="lead">{{ weekLast(lastDay - 4) }}</td>
+                <td class="lead">{{ weekLast(lastDay - 5) }}</td>
+                <td class="lead">{{ weekLast(lastDay - 6) }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script>
@@ -73,6 +78,8 @@ export default {
   },
 
   computed: {
+    /* Some months require 6 rows to display all of their days. This function
+       determines whether or not this is the case. */
     extraRow () {
       if (this.firstDay >= 5 && this.daysInMonth === 31) {
         return true
@@ -93,17 +100,20 @@ export default {
       return Moment().add(this.monthIndex, 'months').endOf('month').format('DD')
     },
     prevMonthLastDay () {
-      return Moment().subtract(this.monthIndex + 1, 'months').endOf('month').format('DD')
+      return Moment().subtract(Math.abs(this.monthIndex) + 1, 'months').endOf('month').format('DD')
     },
     month () {
       return Moment().add(this.monthIndex, 'months').format('MMMM YYYY')
     },
     nextMonth () {
-      return Moment().add(this.monthIndex + 1, 'months').format('MMMM YYYY')
+      return Moment().add(this.monthIndex + 1, 'months').format('MMM YYYY')
     },
     prevMonth () {
-      return Moment().subtract(this.monthIndex + 1, 'months').format('MMMM YYYY')
+      return Moment().subtract(Math.abs(this.monthIndex) + 1, 'months').format('MMM YYYY')
     },
+    /* This function determines the correct day (number) for Saturday of the
+       first week. This value is used to set the values for the days in weeks
+       two thru four (see weekTwo, weekThree, weekFour below) */
     saturdayDay () {
       return this.weekOne(this.firstDay - 6)
     },
@@ -128,6 +138,8 @@ export default {
       }
       return array
     },
+    /* When six rows of weeks are needed for the month, this function calculates
+       the values for week five */
     weekFiveOfSix () {
       let array = []
       for (let i = 22; i < 29; i++) {
@@ -138,6 +150,8 @@ export default {
   },
 
   methods: {
+    /* Determine the dates for the first week, based on the last day of the
+       previous month */
     weekOne (n) {
       let last = this.prevMonthLastDay
       switch (n) {
@@ -169,6 +183,8 @@ export default {
           return 7
       }
     },
+    /* Determine the dates for the last week, based on the last day of the
+       current month */
     weekLast (n) {
       let last = this.currentMonthLastDay
       switch (n) {
